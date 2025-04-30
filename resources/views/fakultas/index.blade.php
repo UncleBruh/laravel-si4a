42cdf8dfd4f3ea0cf936636deb5b47679d5a939a
Nelson <74392133+UncleBruh@users.noreply.github.com>
2025-04-30 16:00:53 +0700

    Initial commit

---
+++ resources/views/fakultas/index.blade.php
@@ -0,0 +1,16 @@
+<h1>fakultas</h1>
+<table>
+    <tr>
+        <th>Nama</th>
+        <th>Singkatan</th>
+        <th>Dekan</th>
+        <th>Wakil Dekan</th>
+    </tr>
+@foreach ($fakultas as $item)
+    <tr>
+        <td> {{ $item->nama }}</td>
+        <td> {{ $item ->singkatan }}</td>
+        <td> {{ $item->dekan }}</td>
+       <td> {{ $item->wakil_dekan }}</td>
+    </tr>
+@endforeach
