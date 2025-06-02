01f917c1fd28b95d1cd32f816b9842b3c923e24d
Nelson <74392133+UncleBruh@users.noreply.github.com>
2025-06-02 13:45:59 +0700

    d

--- app/Http/Controllers/FakultasController.php
+++ app/Http/Controllers/FakultasController.php
@@ -9,86 +9,69 @@ class FakultasController extends Controller
 {
     /**
      * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        // panggil model fakultas menggunakan eloquent
-        $fakultas = Fakultas::all(); // perintah SQL select * from fakultas
-        //dd($fakultas); //dump and die
-        return view('fakultas.index', compact
-        ('fakultas')); //selain compact bisa juga menggunakan with()
+        //panggil model fakultas
+        $fakultas = fakultas::all();//perintah sql select * from fakultas
+        //dd($fakultas);//dump and die
+        return view('fakultas.index', compact('fakultas'));//
+
     }
 
     /**
      * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
      */
     public function create()
     {
+        //
         return view('fakultas.create');
     }
 
     /**
      * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
+        //
         //validasi input
         $input = $request->validate(
             [
-                'nama' => 'required|
-                unique:fakultas',
-                'singkatan' => 'required|max:5',
-                'dekan' => 'required',
-                'wakil_dekan' => 'required',
-            ]);
-
-        //simpan data ke tabel fakultas
-        Fakultas::create($input);
-
+                'nama'=>'required|unique:fakultas',
+                'singkatan'=>'required|Max:5',
+                'dekan'=>'required',
+                'wakil_dekan'=>'required',
+            ]
+        );
+        //simpan data fakultas
+        fakultas::create($input);
         //redirect ke route fakultas.index
-        return redirect()->route('fakultas.index')
-            ->with('success', 'Fakultas berhasil ditambahkan');
+        return redirect()->route('fakultas.index')->with('success', 'Fakultas Berhasil Ditambahkan');
     }
 
     /**
      * Display the specified resource.
-     *
-     * @param  \App\Models\Fakultas  $fakultas
-     * @return \Illuminate\Http\Response
      */
-    public function show(Fakultas $fakultas)
+    public function show($fakultas)
     {
-        $fakultas = Fakultas::findorfail($fakultas);
-        //dd($fakultas); 
+        //
+        $fakultas = Fakultas::findOrFail($fakultas);
+        // dd($fakultas);
         return view('fakultas.show', compact('fakultas'));
     }
 
     /**
      * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Fakultas  $fakultas
-     * @return \Illuminate\Http\Response
      */
     public function edit($fakultas)
     {
-        $fakultas = Fakultas::findorfail($fakultas);
-        //dd($fakultas);
+        $fakultas = Fakultas::findOrFail($fakultas);
+        // dd($fakultas);
         return view('fakultas.edit', compact('fakultas'));
     }
 
     /**
      * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Fakultas  $fakultas
-     * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $fakultas)
     {
@@ -97,7 +80,8 @@ class FakultasController extends Controller
         //validasi input
         $input = $request->validate(
             [
-                'nama' => 'required',
+                'nama' => 'required|
+                unique:fakultas',
                 'singkatan' => 'required|max:5',
                 'dekan' => 'required',
                 'wakil_dekan' => 'required',
@@ -112,21 +96,14 @@ class FakultasController extends Controller
 
     /**
      * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Fakultas  $fakultas
-     * @return \Illuminate\Http\Response
      */
     public function destroy($fakultas)
     {
-        $fakultas = Fakultas::findorfail($fakultas);
-        //dd($fakultas);
+        //
+        $fakultas=Fakultas::findOrFail($fakultas);
 
-
-        //hapus data fakultas
         $fakultas->delete();
 
-        //redirect ke route fakultas.index
-        return redirect()->route('fakultas.index')
-            ->with('success', 'Fakultas berhasil dihapus');
+        return redirect()->route('fakultas.index')->with('success', 'Fakultas berhasil dihapus');
     }
 }
